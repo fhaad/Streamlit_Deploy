5d6618fb9c0612caccd9c68212222d979734c849
fhaad <fhaad25@gmail.com>
2023-01-17 18:54:56 -0500

    cambios

--- app/PF_Olist.py
+++ app/PF_Olist.py
@@ -60,7 +60,7 @@ if uploaded_file:
     dataset = pd.read_csv(uploaded_file)
 #--------------------------------------------------------------------------------------#
 st.subheader('Dataset de Análisis')
-#dataset = pd.read_csv('Datasets/wine_reviews_clean.csv', sep = ',', encoding = 'utf_8')
+dataset = pd.read_csv('Datasets/ventas_ejemplo.csv', sep = ',', encoding = 'utf_8')
 st.dataframe(dataset) # visualiza el dataframe
 #filter = (dataset[['country','price']].groupby(['country']).mean().sort_values(by='price', ascending=False))
 #filter
