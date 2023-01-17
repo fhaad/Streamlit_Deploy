376229ba62b51e6e71cec7cc241c095d511f086f
fhaad <fhaad25@gmail.com>
2023-01-17 12:23:41 -0500

    cambio

--- app/PF_Olist.py
+++ app/PF_Olist.py
@@ -80,7 +80,7 @@ st.header('Visualizacion de Dashboard')
 def Ventas(dataset):
     st.header('Dataset')
     #st.dataframe(dataset)
-    precios_promedio = (dataset.groupby(by=['variety']).avg()[['price']].sort_values(by='price'))
+    precios_promedio = (dataset.groupby(by=['variety']).mean()[['price']].sort_values(by='price'))
 
     fig_precios_promedio = px.bar(
         precios_promedio,
