d06c28f2587c214aa837703cae8788fa57f3b798
fhaad <fhaad25@gmail.com>
2023-01-17 11:56:45 -0500

    cambio

--- app/PF_Olist.py
+++ app/PF_Olist.py
@@ -88,7 +88,7 @@ def Ventas(dataset):
         y = precios_promedio.index,
         orientation="h",
         title="Promedio del Precio"
-        color_discrete_sequence=['#19D3F3'] * len(precios_promedio),
+        color_discrete_sequence=px.colors.qualitative.G10,
         template='plotly_white',
     )
     fig_precios_promedio.update_layout(
