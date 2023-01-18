8ca14ea4726bc83f8b4db6e9673553eec757c39f
fhaad <fhaad25@gmail.com>
2023-01-17 21:22:59 -0500

    cambios

--- app/PF_Olist.py
+++ app/PF_Olist.py
@@ -169,10 +169,10 @@ def kpi():
     st.markdown('***')
     with left_column:
         st.header('Venta Total')
-        st.markdown(f"'{(total_ventas)}'")
+        st.subheader(f"{total_ventas:,}")
     with right_column:
         st.header('Presupuesto Total')
-        st.markdown(f"'{(total_presupuesto)}'")
+        st.markdown(f"{total_presupuesto}")
 
 
 
